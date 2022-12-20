34481d9cd1cfa5f2b9604eb428824c5f7912f6a0
lorenzop <lorenzo@poixson.com>
2022-12-20 17:50:21 -0500

    added more to ComputerPartsPlugin class

--- src/com/poixson/computerparts/ComputerPartsPlugin.java
+++ src/com/poixson/computerparts/ComputerPartsPlugin.java
@@ -1,20 +1,29 @@
 package com.poixson.computerparts;
 
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.poixson.computerparts.listeners.ComputerPartsCommands;
+import com.poixson.computerparts.parts.ComputerPart;
 
 
 public class ComputerPartsPlugin extends JavaPlugin {
 	public static final String LOG_PREFIX  = "[Computer] ";
 	public static final String CHAT_PREFIX = ChatColor.AQUA + "[Computer] " + ChatColor.WHITE;
 	public static final Logger log = Logger.getLogger("Minecraft");
+	protected static final AtomicReference<ComputerPartsPlugin> instance = new AtomicReference<ComputerPartsPlugin>(null);
 
 	// listeners
 	protected final AtomicReference<ComputerPartsCommands> commandListener = new AtomicReference<ComputerPartsCommands>(null);
+
+	protected final CopyOnWriteArraySet<ComputerPart> parts = new CopyOnWriteArraySet<ComputerPart>();
 
 
 
@@ -25,6 +34,8 @@ public class ComputerPartsPlugin extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
+		if (!instance.compareAndSet(null, this))
+			throw new RuntimeException("Plugin instance already enabled?");
 		// commands listener
 		{
 			final ComputerPartsCommands listener = new ComputerPartsCommands(this);
@@ -37,12 +48,30 @@ public class ComputerPartsPlugin extends JavaPlugin {
 
 	@Override
 	public void onDisable() {
+		// unload emulators
+		{
+			final Iterator<ComputerPart> it = this.parts.iterator();
+			while (it.hasNext()) {
+				final ComputerPart part = it.next();
+				part.unload();
+				it.remove();
+			}
+		}
 		// commands listener
 		{
 			final ComputerPartsCommands listener = this.commandListener.getAndSet(null);
 			if (listener != null)
 				listener.unregister();
 		}
+		// stop schedulers
+		try {
+			Bukkit.getScheduler()
+				.cancelTasks(this);
+		} catch (Exception ignore) {}
+		// stop listeners
+		HandlerList.unregisterAll(this);
+		if (!instance.compareAndSet(this, null))
+			throw new RuntimeException("Disable wrong instance of plugin?");
 	}
 
 
