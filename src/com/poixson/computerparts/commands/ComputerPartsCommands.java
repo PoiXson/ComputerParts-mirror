04097d7a2af448bae1b2546962769318ff370f6f
lorenzop <lorenzo@poixson.com>
2023-01-03 04:14:33 -0500

    cleaned up and finished ComputerPartsCommands class using pxnCommandsHandler

--- src/com/poixson/computerparts/commands/ComputerPartsCommands.java
+++ src/com/poixson/computerparts/commands/ComputerPartsCommands.java
@@ -1,70 +1,43 @@
-package com.poixson.computerparts.listeners;
+package com.poixson.computerparts.commands;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.PluginCommand;
-import org.bukkit.entity.Player;
 
+import com.poixson.commonbukkit.tools.commands.pxnCommandsHandler;
 import com.poixson.computerparts.ComputerPartsPlugin;
 
 
-public class ComputerPartsCommands implements CommandExecutor {
-	protected static final String LOG_PREFIX  = ComputerPartsPlugin.LOG_PREFIX;
-	protected static final String CHAT_PREFIX = ComputerPartsPlugin.CHAT_PREFIX;
-
-	protected final ComputerPartsPlugin plugin;
-
-	protected final ArrayList<PluginCommand> cmds = new ArrayList<PluginCommand>();
+public class ComputerPartsCommands extends pxnCommandsHandler {
 
 
 
 	public ComputerPartsCommands (final ComputerPartsPlugin plugin) {
-		this.plugin = plugin;
-	}
-
-
-
-	public void register() {
-		final PluginCommand cmd = this.plugin.getCommand("computer");
-		cmd.setExecutor(this);
-		this.cmds.add(cmd);
-//		cmd.setTabCompleter( new ComputerPartsTabCompleter() );
-	}
-	public void unregister() {
-		for (final PluginCommand cmd : this.cmds) {
-			cmd.setExecutor(null);
-		}
-		this.cmds.clear();
+		super(
+			plugin,
+			"computer",
+			"computerparts",
+			"parts"
+		);
 	}
 
 
 
 	@Override
-	public boolean onCommand(final CommandSender sender, final Command cmd,
+	public List<String> onTabComplete(
+			final CommandSender sender, final Command cmd,
 			final String label, final String[] args) {
-		final Player player = (sender instanceof Player ? (Player)sender : null);
-		final int numargs = args.length;
-		if (numargs >= 1) {
-			switch (args[0]) {
-			case "blink": {
-				if (player == null) {
-					sender.sendMessage(LOG_PREFIX + "Only players can use this command");
-					return true;
-				}
-				if (!player.hasPermission("computer.blink")) {
-					player.sendMessage(CHAT_PREFIX+"You don't have permission to use this.");
-					return true;
-				}
-				this.plugin.toggleBlink(player);
-				return true;
-			}
-			default: break;
-			}
+		final List<String> matches = new ArrayList<String>();
+		final int size = args.length;
+		switch (size) {
+		case 1:
+			if ("blink".startsWith(args[0])) matches.add("blink");
+			break;
+		default:
 		}
-		return false;
+		return matches;
 	}
 
 
